81ebc1ad49d5c9b0c5e9c67a5db12628048967dd
Al DAmico <JavaWiz1@hotmail.com>
2022-06-17 18:16:53 -0400

    project build setup, need to update with poetry

---
+++ setup.py
@@ -0,0 +1,7 @@
+# setup.py
+from setuptools import setup
+
+# This shim delegates the job of doing an editable install to Setuptools’ 
+# legacy mechanism until native support for PEP 660 is available.
+
+setup()
